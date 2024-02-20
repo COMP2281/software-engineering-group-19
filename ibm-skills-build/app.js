a38a00676a193aa1922bcba00558e4679ce611c4
EJ <58092324+ej-b@users.noreply.github.com>
2024-02-20 16:41:27 +0000

    create homepage

--- ibm-skills-build/app.js
+++ ibm-skills-build/app.js
@@ -1,34 +1,31 @@
-const express = require('express');
-const mongoose = require('mongoose');
-require('dotenv').config();
+const express = require('express')
+const mongoose = require('mongoose')
+require('dotenv').config()
+const CourseModel = require('./channel.js')
 const path = require('path');
 
-// Use process.env for sensitive information
 const dbURL = process.env.MONGODB_URI;
 
-// Express app
-const app = express();
+//express app
+const app = express()
 
 app.use(express.json());
+
 app.use(express.static(path.join(__dirname, 'client')));
 
-// Connect to MongoDB Atlas
+//connect to db on mongodb atlas
 mongoose
-  .connect(dbURL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    console.log('Connected to the database');
-  })
-  .catch((error) => {
-    console.log(error);
+    .connect(dbURL)
+    .then(() => {
+        console.info('Connected to the database');
+    })
+    .catch((error) => {
+        console.log(error)
+    })
+
+
+app.get("/", (req, res) => {
+      res.send("Hello, world!");
   });
-
-// Define a simple route for testing
-app.get('/', (req, res) => {
-  res.send('Hello, world!');
-});
-
-// Start the server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+  
+module.exports = app;
