ef94ad2376fb8e0e28b078b08be9af2fce465727
nnxik <gsnb85@dur.ac.uk>
2024-02-25 00:22:22 +0000

    changed the code to add ibm logo to the login page

--- ibm-skills-build/src/pages/login.jsx
+++ ibm-skills-build/src/pages/login.jsx
@@ -5,11 +5,16 @@ const Login = () => {
   return (
     <div className="login-container">
       <div className="brand-section">
-        <h1>IBM SkillsBuild</h1>
+      <img src={require('./Login.png')} alt="IBM Logo" />
       </div>
       <div className="form-section">
         <form className="login-form">
+          <h1>Hello!</h1>
+          <h4>Welcome back</h4>
+
+          <label htmlFor="Email">Email Address: </label>  
           <input type="email" id="email" name="email" placeholder="Email Address" required />
+          <label htmlFor="password">Password: </label>
           <input type="password" id="password" name="password" placeholder="Password" required />
           <button type="submit">Login</button>
           <p className="forgot-password">
