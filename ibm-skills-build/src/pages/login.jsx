c9a6d7a1433e364c5109689394529f51437e2472
Hi-jinks <catherinedoo272@gmail.com>
2024-02-25 22:00:41 +0000

    Untested login with events

--- ibm-skills-build/src/pages/login.jsx
+++ ibm-skills-build/src/pages/login.jsx
@@ -42,20 +42,14 @@ const Login = () => {
       </div>
       <div className="form-section">
         <form className="login-form">
-<<<<<<< HEAD
-          <input type="email" onChange={(e)=>{setEmail(e.target.value)}} id="email" name="email" placeholder="Email Address" required />
-          <input type="password" onChange={(e)=>{setPassword(e.target.value)}} id="password" name="password" placeholder="Password" required />
-          <button type="submit" onClick={submit}>Login</button>
-=======
           <h1>Hello!</h1>
           <h4>Welcome back</h4>
 
           <label htmlFor="Email">Email Address: </label>  
-          <input type="email" id="email" name="email" placeholder="Email Address" required />
+          <input type="email" onChange={(e)=>{setEmail(e.target.value)}} id="email" name="email" placeholder="Email Address" required />
           <label htmlFor="password">Password: </label>
-          <input type="password" id="password" name="password" placeholder="Password" required />
-          <button type="submit">Login</button>
->>>>>>> ef94ad2376fb8e0e28b078b08be9af2fce465727
+          <input type="password" onChange={(e)=>{setPassword(e.target.value)}} id="password" name="password" placeholder="Password" required />
+          <button type="submit" onClick={submit}>Login</button>
           <p className="forgot-password">
             <a href="#">Forgot Password</a>
           </p>
