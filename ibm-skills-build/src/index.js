bd791d896f1d97d418461263ad58cbdd1e2599f3
qasimq123 <45720227+qasimq123@users.noreply.github.com>
2024-03-04 13:58:00 +0000

    Update index.js
    Update

--- ibm-skills-build/src/index.js
+++ ibm-skills-build/src/index.js
@@ -1,7 +1,9 @@
-import ReactDOM from "react-dom";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import "./index.css";
 import LoginPage from "./pages/login";
 import Homepage from "./pages/homepage";
+import Dashboard from "./pages/dashboard";
 import {
   BrowserRouter,
   Routes, 
@@ -15,6 +17,8 @@ root.render(
     <Routes>
       <Route path="/" element={<LoginPage />} />
       <Route path="/homepage" element={<Homepage/>} />
+      <Route path="/dashboard" element={<Dashboard/>} />
+
     </Routes>
   </BrowserRouter>
 );
